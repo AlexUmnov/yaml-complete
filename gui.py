be71d39dc146ed68cac4f4bbe0f3bad92426725a
AlexUmnov <alexvumnov@gmail.com>
2024-09-04 15:20:36 +0200

    minor fix, metrics

--- gui.py
+++ gui.py
@@ -32,7 +32,7 @@ def autocomplete(text, cursor_pos, predictor):
 
 def main(predictor_name='openai_chat_predictor'):
     if predictor_name != "dumb":
-        predictor = predictor_registry[predictor_name]
+        predictor = predictor_registry[predictor_name]()
     else:
         predictor = None
 
